e7fbbcc3e01ff173e6583fb16913dc678ccae392
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-01-16 15:51:16 +0100

    implemented user model

---
+++ backend/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  password: { type: String, required: true },
+  account_balance: { type: Number, required: true },
+  account_name: { type: Number, required: true },
+  userRef: { type: String, default: "" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("user", UserSchema);
